5bc09f40578efb531f7c9016c281febd94c757e6
Shubham Mittal <shubham18101@iiitd.ac.in>
2021-04-12 12:36:59 +0530

    Locks implemented and partial buffer created

--- src/td_ins-td_del.cu
+++ src/td_ins-td_del.cu
@@ -1,12 +1,10 @@
-#include<iostream>
-using namespace std;
+/*
+ * Author: Shubham Mittal
+ * IIITD, 2018101
+ * GPU Project: Accelerating Concurrent Heap on GPUs
+*/
+#include "td_ins-td_del.h"
 
-// BLOCK_SIZE >= BATCH_SIZE always to ensure maximum intra-node parallism and correctness of code
-// For full usage of GPU resources, BLOCK_SIZE = BATCH_SIZE
-#define BATCH_SIZE 1024
-#define BLOCK_SIZE 1024
-// Following code is from https://stackoverflow.com/a/14038590 
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
    if (code != cudaSuccess) 
@@ -16,7 +14,26 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
-// Algorithm referenced from https://wiki.rice.edu/confluence/download/attachments/4435861/comp322-s12-lec28-slides-JMC.pdf?version=1&modificationDate=1333163955158
+__device__ int get_lock_state(int node_idx, int *heap_locks)
+{
+    return heap_locks[node_idx];
+}
+
+__device__ void take_lock(int *lock, int lock_state_1, int lock_state_2)
+{
+    while (atomicCAS(lock, lock_state_1, lock_state_2) != lock_state_1);
+}
+
+__device__ int try_lock(int *lock, int lock_state_1, int lock_state_2)
+{
+    return atomicCAS(lock, lock_state_1, lock_state_2);
+}
+
+__device__ void release_lock(int *lock, int lock_state_1, int lock_state_2)
+{
+    atomicCAS(lock, lock_state_1, lock_state_2);
+}
+
 __device__ void bitonic_sort(int *arr, int size)
 {
     // assuming size = power of 2
@@ -52,42 +69,92 @@ __device__ void bitonic_sort(int *arr, int size)
     }
 }
 
-__global__ void td_insertion(int *items_to_be_inserted, int number_of_items_to_be_inserted)
+__global__ void heap_init(Heap *heap, Partial_Buffer *partial_buffer)
 {
+    int index = threadIdx.x + blockDim.x * blockIdx.x;
+    if(index < HEAP_CAPACITY)
+    {
+        heap -> arr[index] = INT_MAX;
+        if(threadIdx.x < PARTIAL_BUFFER_CAPACITY)
+            partial_buffer -> arr[threadIdx.x] = INT_MAX;   
+    }
+}
+
+__global__ void td_insertion(int *items_to_be_inserted, 
+                             int number_of_items_to_be_inserted, 
+                             int *heap_locks, 
+                             Partial_Buffer *partial_buffer,
+                            Heap *heap)
+{
+    /*
+     * number_of_items_to_be_inserted <= BATCH_SIZE
+    */
     __shared__ int items_to_be_inserted_shared_mem[BATCH_SIZE];
     int my_thread_id = threadIdx.x;
-
-    if (my_thread_id < BATCH_SIZE)
+    if (my_thread_id < number_of_items_to_be_inserted)
     {
         items_to_be_inserted_shared_mem[my_thread_id] = items_to_be_inserted[my_thread_id];
     }
-    __syncthreads();
-    bitonic_sort(items_to_be_inserted_shared_mem, number_of_items_to_be_inserted);
-    
-    if (my_thread_id < BATCH_SIZE)
+    else if(my_thread_id < BATCH_SIZE)
     {
-        items_to_be_inserted[my_thread_id] = items_to_be_inserted_shared_mem[my_thread_id];
+        items_to_be_inserted_shared_mem[my_thread_id] = INT_MAX;
+    }
+    __syncthreads();
+
+    bitonic_sort(items_to_be_inserted_shared_mem, number_of_items_to_be_inserted);
+
+    int root_node_idx = 0;
+    if (my_thread_id == 0)
+        take_lock(&heap_locks[root_node_idx], AVAILABLE, INUSE);
+
+    if (partial_buffer -> size + number_of_items_to_be_inserted >= BATCH_SIZE)
+    {
+
+    }
+    else
+    {
+        release_lock(&heap_locks[root_node_idx], AVAILABLE, INUSE);
+        return;
     }
 
 }
 
-int main()
+void heap_init()
 {
+    Heap *d_heap;
+    Partial_Buffer *d_partial_buffer;
+
+    gpuErrchk( cudaMalloc(&d_partial_buffer, sizeof(Partial_Buffer)));
+    gpuErrchk( cudaMalloc(&d_heap, sizeof(Heap))); // need to fill with INT_MAX
+
+    heap_init<<<ceil(HEAP_CAPACITY/1024.0), 1024>>>(d_heap, d_partial_buffer);
+
+    int *d_heap_locks;
+    gpuErrchk( cudaMalloc((void**)&d_heap_locks, NUMBER_OF_NODES * sizeof(int)) );
+    gpuErrchk( cudaMemset(d_heap_locks, AVAILABLE, NUMBER_OF_NODES * sizeof(int)) );
+
+
+    
+    
+
+
     srand(time(NULL));
     int n = 1024;
     int h_arr[n];
     for(int i = 0 ; i < n ; i++)
         h_arr[i] = rand() % 10;
+        
     int *d_arr;
     cudaMalloc((void**)&d_arr, n * sizeof(int));
     cudaMemcpy(d_arr, h_arr, n * sizeof(int), cudaMemcpyHostToDevice);
-    td_insertion<<<1, BLOCK_SIZE>>>(d_arr, n);
+    td_insertion<<<1, BLOCK_SIZE>>>(d_arr, n, d_heap_locks, d_partial_buffer, d_heap);
 	cudaDeviceSynchronize();
     cudaMemcpy(h_arr, d_arr, n * sizeof(int), cudaMemcpyDeviceToHost);
-    bool correct = true;
-    for(int i = 1 ; i < n ; i++)
-        if(h_arr[i] < h_arr[i-1])
-            correct = 0;
-    cout << correct <<"\n";
+    // for(int i = 0 ;i < 6 ;i++)
+    // cout << h_arr[i]<<" ";
+}
 
+int main()
+{
+    heap_init();
 }
---
+++ src/td_ins-td_del.h
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+/* 
+ * ALERT: BLOCK_SIZE >= BATCH_SIZE always to ensure correctness of code
+ * For full usage of GPU resources, BLOCK_SIZE = BATCH_SIZE 
+ * BATCH_SIZE should be power of 2
+ */
+
+#define BATCH_SIZE 1024
+#define BLOCK_SIZE 1024
+#define PARTIAL_BUFFER_CAPACITY (BATCH_SIZE - 1)
+#define NUMBER_OF_NODES 10
+#define HEAP_CAPACITY NUMBER_OF_NODES * BATCH_SIZE
+
+enum LOCK_STATES {AVAILABLE, INUSE, TARGET, MARKED};
+
+// Code taken from https://stackoverflow.com/a/14038590
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+
+struct Partial_Buffer
+{
+    int size = 0;
+    int arr[PARTIAL_BUFFER_CAPACITY];
+};
+
+struct Heap
+{
+    int size = 0;
+    int arr[HEAP_CAPACITY];
+};
+
+__device__ int get_lock_state(int node_idx, int *heap_locks);
+__device__ void take_lock(int *lock, int lock_state_1, int lock_state_2);
+__device__ int try_lock(int *lock, int lock_state_1, int lock_state_2);
+__device__ void release_lock(int *lock, int lock_state_1, int lock_state_2);
+
+// Algorithm referenced from https://wiki.rice.edu/confluence/download/attachments/4435861/comp322-s12-lec28-slides-JMC.pdf?version=1&modificationDate=1333163955158
+__device__ void bitonic_sort(int *arr, int size);
+
+__global__ void heap_init(Heap *heap);
+__global__ void td_insertion(int *items_to_be_inserted, int number_of_items_to_be_inserted, int *heap_locks, Partial_Buffer *partial_buffer, Heap *heap);
+
+void heap_init();
